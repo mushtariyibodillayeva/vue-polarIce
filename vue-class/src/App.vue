fd691cebdc71462626ad38720b2bd5d79687575f
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2023-02-20 18:09:31 +0500

    vue-polarIce

---
+++ vue-class/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <header class="header">
+      <a href="#"> <img src="./components/logo.svg" alt="logo" /></a>
+      <ul class="header-menu">
+        <li class="header-item">
+          <a href="#" class="header-link">Home</a>
+        </li>
+        <li class="header-item">
+          <a href="#" class="header-link">Product</a>
+        </li>
+        <li class="header-item">
+          <a href="#" class="header-link">Pricing</a>
+        </li>
+        <li class="header-item">
+          <a href="#" class="header-link">Testimonials</a>
+        </li>
+        <li class="header-item">
+          <a href="#" class="header-link">Contact Us</a>
+        </li>
+      </ul>
+      <button class="header-btn">Buy Now</button>
+    </header>
+    <div class="citeAbout">
+      <div class="citeLeft">
+        <p class="text1">Sweet fun, full of milk.</p>
+        <h1>Feel inside cold with our delicious ice-cream.</h1>
+        <p class="text2">
+          Some food has looked so awful that it's looked like something that the
+          dog's brought home, yet after one mouthful I've been left eating my
+          thoughts, my words.
+        </p>
+        <button class="left-btn">Buy Now</button>
+      </div>
+      <div class="citeRight">
+        <img src="./components/ice.svg" alt="iceCream" />
+      </div>
+    </div>
+  </div>
+</template>
+<script></script>
+<style></style>
