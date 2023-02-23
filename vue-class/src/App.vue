50a73a3683bfb1fcf45aee96d364435e97c6178b
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2023-02-23 18:27:47 +0500

    new

--- vue-class/src/App.vue
+++ vue-class/src/App.vue
@@ -36,7 +36,61 @@
         <img src="./components/ice.svg" alt="iceCream" />
       </div>
     </div>
+    <hr />
+    <div class="purchases">
+      <div class="card1">
+        <h2 class="title">A delicious dessert</h2>
+        <img
+          src="./components/cream1.svg"
+          alt="cream"
+          style="width: 350px; height: 350px"
+        />
+        <h3 class="add-purchases">
+          Purchases: <span>{{ obj.count }}</span>
+        </h3>
+        <button @click="increment" class="card-button">Add</button>
+        <button @click="reset" class="card-button2">Reset</button>
+      </div>
+      <div class="card2">
+        <h2 class="title">Cayenne chocolate</h2>
+        <img
+          src="./components/cream2.svg"
+          alt="cream"
+          style="width: 350px; height: 350px"
+        />
+        <h3 class="add-purchases">
+          Purchases: <span>{{ obj.count }}</span>
+        </h3>
+        <button @click="increment" class="card-button">Add</button>
+        <button @click="reset" class="card-button2">Reset</button>
+      </div>
+      <div class="card3">
+        <h2 class="title">Ice cream dessert</h2>
+        <img
+          src="./components/cream3.svg"
+          alt="cream"
+          style="width: 350px; height: 350px"
+        />
+        <h3 class="add-purchases">
+          Purchases: <span>{{ obj.count }}</span>
+        </h3>
+        <button @click="increment" class="card-button">Add</button>
+        <button @click="reset" class="card-button2">Reset</button>
+      </div>
+    </div>
   </div>
 </template>
-<script></script>
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const obj = reactive({ count: 0 });
+
+const increment = () => {
+  obj.count++;
+};
+
+const reset = () => {
+  obj.count = 0;
+};
+</script>
 <style></style>
